eba28dc8796f116e18fe1472ba51ad8571e066d5
Carl Stephens <carlst99@outlook.co.nz>
2019-11-16 13:22:36 +1300

    Add preferences realm object and dark mode preference

---
+++ Vault.Core/Model/DbContext/Preferences.cs
@@ -0,0 +1,10 @@
+﻿using Realms;
+
+namespace Vault.Core.Model.DbContext
+{
+    public class Preferences : RealmObject
+    {
+        public bool DarkModeEnabled { get; set; }
+        public int ThumbnailSize { get; set; }
+    }
+}
--- Vault.Wpf/Views/HubView.xaml.cs
+++ Vault.Wpf/Views/HubView.xaml.cs
@@ -1,24 +1,39 @@
 ﻿using MaterialDesignThemes.Wpf;
 using MvvmCross.Platforms.Wpf.Views;
+using Realms;
 using System;
+using Vault.Core.Model.DbContext;
 
 namespace Vault.Wpf.Views
 {
     public partial class HubView : MvxWpfView
     {
+        private readonly Realm _realm;
+        private readonly Preferences _preferences;
+
         public HubView()
         {
             InitializeComponent();
+            _realm = RealmHelpers.GetRealmInstance();
+            _preferences = RealmHelpers.GetPreferences(_realm);
+
+            if (_preferences.DarkModeEnabled)
+            {
+                EnableDarkMode(this, null);
+                TglBtnDarkMode.IsChecked = true;
+            }
         }
 
-        private void ToggleButton_Checked(object sender, System.Windows.RoutedEventArgs e)
+        private void EnableDarkMode(object sender, System.Windows.RoutedEventArgs e)
         {
             ModifyTheme(t => t.SetBaseTheme(Theme.Dark));
+            _realm.Write(() => _preferences.DarkModeEnabled = true);
         }
 
-        private void ToggleButton_Unchecked(object sender, System.Windows.RoutedEventArgs e)
+        private void DisableDarkMode(object sender, System.Windows.RoutedEventArgs e)
         {
             ModifyTheme(t => t.SetBaseTheme(Theme.Light));
+            _realm.Write(() => _preferences.DarkModeEnabled = false);
         }
 
         private static void ModifyTheme(Action<ITheme> modificationAction)
