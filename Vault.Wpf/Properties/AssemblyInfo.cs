cb41429c679cef50be468830e4be26379befc748
Carl Stephens <carlst99@outlook.co.nz>
2019-11-19 18:48:12 +1300

    Update assembly info

--- Vault.Wpf/Properties/AssemblyInfo.cs
+++ Vault.Wpf/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("Vault")]
 [assembly: AssemblyDescription("A secure media manager")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Carl Stephens")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Vault")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
