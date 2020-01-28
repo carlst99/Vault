aa9b89780566fbc08f9eb9d6ff6ab16327bc7a7a
Carl Stephens <carlst99@outlook.co.nz>
2020-01-28 21:28:20 +1300

    Expand image on mouse double click

--- Vault.Wpf/Views/ImageDisplayView.xaml.cs
+++ Vault.Wpf/Views/ImageDisplayView.xaml.cs
@@ -3,6 +3,7 @@ using MvvmCrossExtensions.Wpf.Presenters.MasterDetail;
 using Realms;
 using System.Windows;
 using Vault.Core.Model.DbContext;
+using Vault.Core.ViewModels;
 
 namespace Vault.Wpf.Views
 {
@@ -24,6 +25,15 @@ namespace Vault.Wpf.Views
         private void SldrThumbnailSize_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             _realmInstance?.Write(() => _userPreferences.ImageThumbnailSize = (int)e.NewValue);
+        }
+
+        private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            // Handle any double clicks
+            if (e.ClickCount == 2)
+            {
+                ((ImageDisplayViewModel)ViewModel).OpenDialogCommand.Execute();
+            }
         }
     }
 }
